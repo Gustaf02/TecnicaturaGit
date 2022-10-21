e6ff1f9ae9dcf50f423ef325efcacddc44089ab8
Gustaf02 <92409193+Gustaf02@users.noreply.github.com>
2022-10-21 09:23:33 -0300

    Últimos cambios en JS

---
+++ Javascript/Leccion02/02-01-ConcatenacionDeCadenas.js
@@ -0,0 +1,14 @@
+var nombre = 'José';
+var apellido = 'Montés';
+var nombreCompleto = nombre + apellido;
+console.log(nombre);
+var nombreCompleto2 = 'Ariel' + ' '+ 'Betancud';
+console.log(nombreCompleto);
+var juntos = nombre + 219;
+console.log(juntos);
+juntos =  nombre + 17 + 18;  // Lee de izquierda a derecha
+console.log(juntos)
+juntos = 17 + 18 + nombre;
+console.log(juntos);
+nombre += apellido;
+console.log(nombre);
