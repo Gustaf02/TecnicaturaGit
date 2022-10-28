d37174eeffb004cf395f83e655dfd054a20f1ecd
Gustaf02 <92409193+Gustaf02@users.noreply.github.com>
2022-10-28 02:48:25 -0300

    Cambios en JavaScript

--- Javascript/Leccion02/02-01-ConcatenacionDeCadenas.js
+++ Javascript/Leccion02/02-01-ConcatenacionDeCadenas.js
@@ -12,3 +12,21 @@ juntos = 17 + 18 + nombre;
 console.log(juntos);
 nombre += apellido;
 console.log(nombre);
+
+//hoy ya no se usa var, se usa let y const
+let nombre2 = "Pedro";
+console.log(nombre2);
+const apellido2 = "Gómez";
+console.log(apellido2)
+
+let x, y; // se pueden crear varias variables dentro de una misma línea
+x= 17, y =21;  // se puede hacer asignación de variables dentro de la misma línea
+let z = x + y; // se asigna el valor de la operación
+console.log(z);
+
+let _1num = 31;  // no utilizar números para iniciar el valor de una variable
+let rompiendo = "rompe" // no utilizar palabras reservadas para variables
+
+console.log(_1num)
+console.log(rompiendo)
+
---
+++ Javascript/Leccion03/03-01-OperadoresJavaScript.js
@@ -0,0 +1,14 @@
+// Ejercicio para encontrar números pares e impares
+let parInpar = 5;
+if (parInpar % 2 == 0){
+    console.log("Es un número par")}
+else{
+    console.log("Es un número impar")
+}
+
+// Ejercicio es mayor de edad
+let edad = 10, adulto = 18;
+if (edad >= 18){
+  console.log("Es una persona adulta")}
+  else{
+    console.log("Es menor de edad")}
