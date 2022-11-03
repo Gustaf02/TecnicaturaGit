b50cb5ebf858bafa9c730c5ff07fdd8c77945ccc
Gustaf02 <92409193+Gustaf02@users.noreply.github.com>
2022-11-03 12:46:36 -0300

    Operadores en JavaSript y Ejercicios

--- Javascript/Leccion03/03-01-OperadoresJavaScript.js
+++ Javascript/Leccion03/03-01-OperadoresJavaScript.js
@@ -12,3 +12,14 @@ if (edad >= 18){
   console.log("Es una persona adulta")}
   else{
     console.log("Es menor de edad")}
+
+
+// Ejercicio dentro de un rango
+let dentroRango = 5;  // Aquí iremos cambiando el valor
+let valMin = 0; valMax = 10;
+if (dentroRango >= valMin && dentroRango <= valMax){
+  console.log('Está dentro del rango establecido')
+}
+else{
+  console.log('Está fuera del rango establecido')
+}
