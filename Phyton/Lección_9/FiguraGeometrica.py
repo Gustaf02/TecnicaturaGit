4c4515db48a96888d6dbb1fc6fae7c0460d8af83
Gustaf02 <92409193+Gustaf02@users.noreply.github.com>
2022-11-10 02:00:48 -0300

    Clase 13 de Python
    hablamos de validaciones. Método encapsulado, validaciones en setters. Clases abstractas y el comienzo del contexto estático.

--- Phyton/Lección_9/FiguraGeometrica.py
+++ Phyton/Lección_9/FiguraGeometrica.py
@@ -1,15 +1,29 @@
-class FiguraGeometrica:
+from abc import ABC, abstractmethod
+# ABC significa: Abstract Base Class, convierte una clase en abstracta
+class FiguraGeometrica(ABC):  # eS hija de ABC
     def __init__(self, ancho, alto):
-        self._ancho = ancho           # _ancho se encapsula, usamos entonces get y set
-        self._alto = alto
+
+        if self._validar_valores(ancho):
+            self._ancho = ancho  # _ancho se encapsula, usamos entonces get y set
+        else:
+            self._ancho = 0
+            print(f'El valor es erróneo para el ancho {ancho}')
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            self._ancho = 0
+            print(f'El valor es erróneo para el alto {alto}')
 
     @property
     def ancho(self):
-             return self._ancho
+        return self._ancho
 
     @ancho.setter
     def ancho(self, ancho):
-        self._ancho = ancho
+        if self._validar_valores(ancho):
+            self._ancho = ancho
+        else:
+            print(f'dato erroneo para el ancho{ancho}')
 
     @property
     def alto(self):
@@ -17,8 +31,17 @@ class FiguraGeometrica:
 
     @alto.setter
     def alto(self, alto):
-        self._alto = alto
+        if self._validar_valores(alto):
+            self._alto = alto
+        else:
+            print(f'dato erroneo para el alto{alto}')
+
+    @abstractmethod  # hace que en cuadrado, rectángulo sea obligatorio que esté el método
+    def calcular_area(self):
+        pass
 
     def __str__(self):
         return f'Figura geométrica [Ancho: {self._ancho}, Alto: {self._alto}]'
 
+    def _validar_valores(self, valor):  # Método encapsulado
+        return True if 0 < valor < 10 else False
--- Phyton/Lección_9/test_figura_geometrica.py
+++ Phyton/Lección_9/test_figura_geometrica.py
@@ -1,16 +1,26 @@
 from Cuadrado import Cuadrado
+from FiguraGeometrica import FiguraGeometrica
 from Rectangulo import Rectangulo
 
+print('Creación de objeto clase cuadrado'.center(50,'_'))
+
 cuadrado1 = Cuadrado(5, 'Azul')
-print(cuadrado1.ancho)
-print(cuadrado1.alto)
+cuadrado1.alto = -10
+#  print(cuadrado1.ancho)
+#  print(cuadrado1.alto)
 print(f'Cálculo del área del cuadrado: {cuadrado1.calcular_area()}')
 
 # MRO = Method Resolution Order
 print(Cuadrado.mro())
 
 print(cuadrado1)
-
+print('Creación de objeto clase rectángulo'.center(50,'_'))
 rectangulo1 = Rectangulo(3, 8, 'verde')
+rectangulo1.ancho = 15
+
 print(f'Cálculo del área del rectángulo  {rectangulo1.calcular_area()}')
 print(rectangulo1)
+
+# figura1 = FiguraGeometrica()   # Pide importar  No se puede instanciar. Es abstracta.
+
+print(Cuadrado.mro())
